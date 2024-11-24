6683e7ba88b4967cbe9feb2a58c6a45a292b6014
romakot321 <play62342@gmail.com>
2024-11-24 01:53:50 +0000

    add score

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 //Define globals
 var canvas = document.getElementById('canvas');
+var scoreElement = document.getElementById('score');
 var ctx = canvas.getContext('2d');
 var playerX = 50;
 var playerY = 50;
+var score = 0;
+
+var objs = [];
 
 canvas.addEventListener('touchstart', function (event) {
     touchstartX = event.changedTouches[0].screenX;
@@ -44,11 +48,18 @@ function resizeCtxCanvas(ctx) {
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
 
+function addToScore(val) {
+  score += val;
+  scoreElement.innerText = "Score: " + score;
+}
+
 const app = window.Telegram.WebApp;
 app.ready()
 app.disableVerticalSwipes();
 
 resizeCtxCanvas(ctx);
+generate();
+generate();
 draw();
 
 function move(direction) {
@@ -75,11 +86,47 @@ function move(direction) {
   draw();
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generate() {
+  var obj = {
+    x: getRandomInt(0, ctx.canvas.width - 100),
+    y: getRandomInt(0, ctx.canvas.height - 100)
+  }
+  objs.push(obj);
+}
+
+function update() {
+  objs.forEach(element => {
+    if (element.x <= playerX && playerX <= element.x + 100 && element.y <= playerY && playerY <= element.y + 100) {
+      objs = objs.filter(item => item !== element);
+      addToScore(1);
+      generate();
+      draw();
+      console.log("Score:", score)
+    }
+  });
+}
+
 
 function draw() {
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
+  ctx.strokeStyle = "black";
   ctx.beginPath();
   ctx.rect(playerX, playerY, 50, 50);
   ctx.stroke();
+
+  ctx.strokeStyle = "red";
+  objs.forEach(element => {
+    ctx.beginPath();
+    ctx.rect(element.x, element.y, 100, 100);
+    ctx.stroke();
+  });
+
+  update();
 }
